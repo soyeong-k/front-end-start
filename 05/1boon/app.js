679f20d4b7b56a26cdc53a655865eef9b55368e6
soyeong-k <thdud7969@naver.com>
2018-10-12 00:06:26 +0900

    5차

--- 05/1boon/app.js
+++ 05/1boon/app.js
@@ -1,31 +1,71 @@
+// console.log('1boon');
+
+// var url = 'https://1boon.kakao.com/ch/enter.json?page=1&pagesize=10';
+
+// getUrlData(url, print);
+
+// function print(json) {
+//   console.log(json);
+
+//   var str = '';
+
+//   for (var i = 0; i < json.data.length; i++) {
+//     var title = json.data[i].title;
+//     var path = json.data[i].path;
+
+//     // str +=
+//     //   '<a href="https://1boon.kakao.com/' +
+//     //   path +
+//     //   '" target="_blank">' +
+//     //   title +
+//     //   '</a><br>';
+
+//     //https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Template_literals
+//     str += `<a href="http://1boon.kakao.com/${path}" target="_blank">${title}</a><br>`;  
+//   }
+
+//   document.getElementById('wrap').innerHTML = str;
+// }
+
+
+// function getUrlData(url, callback) {
+//   fetch(url)
+//     .then(function(response) {
+//       response.json().then(function(data) {
+//         // console.log('json data:', data);
+//         callback(data);
+//       });
+//     })
+//     .catch(function(err) {
+//       console.log('Fetch Error :-S', err);
+//     });
+// }
+
 console.log('1boon');
 
+var count = 1;
 var url = 'https://1boon.kakao.com/ch/enter.json?page=1&pagesize=10';
+var btn = document.getElementById('btn');
 
-getUrlData(url, print);
 
-function print(json) {
-  console.log(json);
+
+function print(json){
+  console.log( json );
 
   var str = '';
 
-  for (var i = 0; i < json.data.length; i++) {
+  for(var i=0; i < json.data.length; i++){
+     // console.log(json.data[i].title);
+
     var title = json.data[i].title;
-    var path = json.data[i].path;
+    var path =json.data[i].path; 
+    str += '<a href="https://1boon.kakao.com/'+path+'">' + title + '</a><br>';
 
-    // str +=
-    //   '<a href="https://1boon.kakao.com/' +
-    //   path +
-    //   '" target="_blank">' +
-    //   title +
-    //   '</a><br>';
 
-    //https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Template_literals
-    str += `<a href="http://1boon.kakao.com/${path}" target="_blank">${title}</a><br>`;  
   }
-
-  document.getElementById('wrap').innerHTML = str;
+  document.getElementById('wrap').innerHTML += str;
 }
+
 
 
 function getUrlData(url, callback) {
@@ -33,10 +73,25 @@ function getUrlData(url, callback) {
     .then(function(response) {
       response.json().then(function(data) {
         // console.log('json data:', data);
-        callback(data);
+
+        callback(data)
       });
     })
+
     .catch(function(err) {
       console.log('Fetch Error :-S', err);
+
     });
+    
 }
+
+
+
+btn.addEventListener('click',function(){
+
+  count++;
+  var url = "https://1boon.kakao.com/ch/enter.json?page="+count+"&pagesize=10";
+
+  getUrlData(url,print);
+
+});
